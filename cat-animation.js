27bd36ac1a7351eb0d5586db92260f0bb33142cb
Kamila <118283673+mazurekkamila@users.noreply.github.com>
2023-02-04 23:50:35 +0100

    Add tail movements

--- cat-animation.js
+++ cat-animation.js
@@ -1,1 +1,22 @@
 
+const cat = document.querySelector("#cat");
+const tail = document.querySelector(".tail");
+
+let tailDirection = 1;
+
+setInterval(() => {
+  const currentRotation = tail.style.transform.replace(/[^0-9]/g, '');
+  const newRotation = +currentRotation + (10 * tailDirection);
+  
+  if (newRotation >= 135 || newRotation <= 45) {
+    tailDirection *= -1;
+  }
+  
+  tail.style.transform = `rotate(${newRotation}deg)`;
+}, 50);
+
+setInterval(() => {
+  cat.classList.toggle("blink");
+}, 500);
+
+
